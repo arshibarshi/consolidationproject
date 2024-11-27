5333a09e85eca5a73d2c32a370d8bed4be65a3ce
Ademir Ferreyra <adefer@terpmail.umd.edu>
2024-11-26 23:02:46 -0500

    initial commit, startited with a minimal commit ignore

---
+++ consolidation_project.py
@@ -0,0 +1,31 @@
+#Consolidation Project - Ademir Ferreyra
+#Dice game 
+
+import os
+import random
+
+
+#Start by calling variables
+
+#Player(p) scores 
+p1 = 0
+p2 = 0
+
+#dice
+dice1 = [1,2,3,4,5,6]
+dice2 = [1,2,3,4,5,6]
+dice3 = [1,2,3,4,5,6]
+
+
+
+#Dice roll
+diceRolls = [dice1, dice2, dice3]
+
+for x in diceRolls:
+    print(x)
+
+#shuffling through the dice 
+#probably has a for loop to work through the 3 dice rolls
+#uses the shuffle command to "roll" the dice 
+
+
